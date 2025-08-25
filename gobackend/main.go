415f0f3253a7879f43acfbed6ca931773414b5f6
bbackov <backovborna@gmail.com>
2025-08-25 23:26:29 +0200

    ispravak bugova: frontend + backend

--- gobackend/main.go
+++ gobackend/main.go
@@ -84,7 +84,22 @@ func main() {
 	})
 
 	fmt.Println("Server pokrenut na 0.0.0.0:8085")
-	http.ListenAndServe("0.0.0.0:8085", nil)
+	http.ListenAndServe("0.0.0.0:8085", corsMiddleware(http.DefaultServeMux))
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
 
 func jsonError(w http.ResponseWriter, status int, poruka string) {
